164c3300fd2058e9583f24f47c64bc923186ec40
deadzy <cocofaudel@gmail.com>
2020-06-20 08:59:58 +0200

    Update sm.js

--- js/sm.js
+++ js/sm.js
@@ -1,6 +1,6 @@
 $.ajax(
 {
-	url: 'https://steemmonsters.com/market/for_sale_grouped/',
+	url: 'https://game-api.splinterlands.com/market/for_sale_grouped/',
 	dataType: 'json',
 	type : 'GET',
 	success: function(datas)
@@ -17,7 +17,7 @@ function getCardInfo()
 	let totalBCX = 0;
 	$.ajax(
 	{
-		url: 'https://steemmonsters.com/cards/get_details/',
+		url: 'https://game-api.splinterlands.com/cards/get_details/',
 		dataType: 'json',
 		type : 'GET',
 		success: function(d)
